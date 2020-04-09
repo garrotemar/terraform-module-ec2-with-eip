0a89627e3ad0f5eff02c86f9e35da44d62dd9c96
Marcelo Garrote <marcelo.garrote@gmail.com>
2020-04-09 10:38:57 -0300

    Fixed description on sg.tf and replaced name by name_prefix

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "sg" {
-  name        = "${var.project_name}-${var.environment}"
-  description = "Segurity Group for ${var.project_name}-${var.environment}"
+  name_prefix        = "${var.project_name}-${var.environment}-"
+  description = "Security Group for ${var.project_name}-${var.environment}"
   vpc_id      = var.vpc_id
 
   ingress {
